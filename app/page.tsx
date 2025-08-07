a65c38b728110e1d57091505676e599aadfa39d7
janfdev <fauzanrizqinoor@gmail.com>
2025-08-07 10:04:32 +0700

    Remove unused sections from Home component and clean up imports in ActivitySection

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import ActivitySection from "@/components/section/activity-section";
-import BlogSection from "@/components/section/blog-section";
-import ContentSection from "@/components/section/content-section";
 import Header from "@/components/section/header";
 import HeroSection from "@/components/section/hero-section";
-import MajorSection from "@/components/section/major-section";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <HeroSection />
-      <ContentSection />
-      <BlogSection />
-      <MajorSection />
-      <ActivitySection />
     </main>
   );
 }
--- components/section/activity-section.tsx
+++ components/section/activity-section.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 import Dummy from "@/public/assets/dummy.jpg";
 import { Button } from "../ui/button";
